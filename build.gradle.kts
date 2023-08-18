9332a871981a99eb3a2fe54f3d1d52d47c746365
Harsh Shandilya <me@msfjarvis.dev>
2023-08-19 02:15:39 +0530

    fix(build): exclude generated sources from Spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,12 @@ plugins { alias(libs.plugins.spotless) }
 spotless {
   kotlin {
     target("**/*.kt")
+    targetExclude("**/build/")
     ktfmt().googleStyle()
   }
   kotlinGradle {
     target("**/*.kts")
+    targetExclude("**/build/")
     ktfmt().googleStyle()
   }
 }
