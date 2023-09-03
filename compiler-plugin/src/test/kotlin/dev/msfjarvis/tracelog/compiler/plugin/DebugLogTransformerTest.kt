e8648924b25cda1c2e9282a9bb289748273e6a75
Harsh Shandilya <me@msfjarvis.dev>
2023-09-03 19:53:19 +0530

    feat(compiler-plugin): add a failing test for nested logger functions

--- compiler-plugin/src/test/kotlin/dev/msfjarvis/tracelog/compiler/plugin/DebugLogTransformerTest.kt
+++ compiler-plugin/src/test/kotlin/dev/msfjarvis/tracelog/compiler/plugin/DebugLogTransformerTest.kt
@@ -8,7 +8,7 @@ import com.tschuchort.compiletesting.SourceFile.Companion.kotlin
 import org.jetbrains.kotlin.compiler.plugin.CliOption
 import org.jetbrains.kotlin.compiler.plugin.CommandLineProcessor
 import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 @OptIn(ExperimentalCompilerApi::class)
@@ -88,7 +88,7 @@ class DebugLogTransformerTest {
           messageOutputStream = System.out
         }
         .compile()
-    assertEquals(ExitCode.OK, result.exitCode)
+    assertThat(result.exitCode).isEqualTo(ExitCode.OK)
 
     val kClazz = result.classLoader.loadClass("SourceFileKt")
     val transformableWithReturnValue =
@@ -97,6 +97,115 @@ class DebugLogTransformerTest {
     assertThat(retVal).isInstanceOf(String::class.java)
     assertThat(retVal).isEqualTo("Return value!")
     val msgField = kClazz.declaredFields.first { it.name == "messages" }
+    msgField.isAccessible = true
+    val messages = msgField.get(null) as? List<*>
+    assertThat(messages).isNotNull()
+    assertThat(messages).contains("⇢ transformableWithReturnValue()")
+    assertThat((messages?.last() as? String)?.replace("\\[.*]".toRegex(), "[]"))
+      .isEqualTo(
+        "⇠ transformableWithReturnValue [] = Return value!",
+      )
+  }
+
+  @Disabled
+  @Test
+  fun `nested logger function fails to resolve`() {
+    val annotationSrcFile =
+      kotlin(
+        "DebugLog.kt",
+        """
+      package dev.msfjarvis.annotation
+      annotation class DebugLog
+    """
+          .trimIndent()
+      )
+    val srcFile =
+      kotlin(
+        "SourceFile.kt",
+        """
+      import dev.msfjarvis.annotation.DebugLog
+      
+      @DebugLog
+      fun transformable() {
+        println("In a transformable function!")
+      }
+      
+      @DebugLog
+      fun transformableWithReturnValue(): String {
+        println("In a transformable function!")
+        return "Return value!"
+      }
+      
+      fun nonTransformable() {
+        println("Not in a transformable function!")
+      }
+      
+      class TracingTest {
+        @DebugLog
+        fun transformableInClass() {
+          println("In a transformable function!")
+        }
+        
+        fun nonTransformable() {
+          println("Not in a transformable function!")
+        }
+      }
+    """
+          .trimIndent()
+      )
+    val loggerFile =
+      kotlin(
+        "Logger.kt",
+        """
+      object Logger {
+        val messages = mutableListOf<String>()
+      
+        fun recordMessage(message: Any?) {
+          messages += message.toString()
+        }
+      }
+    """
+          .trimIndent()
+      )
+
+    val result =
+      KotlinCompilation()
+        .apply {
+          val processor = TracingCommandLineProcessor()
+          pluginOptions = buildList {
+            add(
+              processor.option(
+                TracingCommandLineProcessor.OPTION_LOGGER_FUNCTION,
+                "Logger.recordMessage"
+              )
+            )
+            add(
+              processor.option(
+                TracingCommandLineProcessor.OPTION_ANNOTATION_NAME,
+                "dev/msfjarvis/annotation/DebugLog"
+              )
+            )
+          }
+          sources = listOf(annotationSrcFile, srcFile, loggerFile)
+          compilerPluginRegistrars = listOf(TracingCompilerPluginRegistrar())
+          commandLineProcessors = listOf(processor)
+          noOptimize = true
+
+          inheritClassPath = true
+          messageOutputStream = System.out
+        }
+        .compile()
+    assertThat(result.exitCode).isEqualTo(ExitCode.OK)
+
+    val kClazz = result.classLoader.loadClass("SourceFileKt")
+    val transformableWithReturnValue =
+      kClazz.declaredMethods.first { it.name == "transformableWithReturnValue" }
+    val retVal = transformableWithReturnValue.invoke(null)
+    assertThat(retVal).isInstanceOf(String::class.java)
+    assertThat(retVal).isEqualTo("Return value!")
+
+    val loggerClazz = result.classLoader.loadClass("LoggerKt")
+    val msgField = loggerClazz.declaredFields.first { it.name == "messages" }
     msgField.isAccessible = true
     val messages = msgField.get(null) as? List<*>
     assertThat(messages).isNotNull()
